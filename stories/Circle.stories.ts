07ac8c7c11d0d6b33ff1b8bf8c0c4c54fb72d379
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-06 23:30:38 +0900

    storybookにCircleを追加

---
+++ stories/Circle.stories.ts
@@ -0,0 +1,11 @@
+import Circle from "@/stories/Circle";
+import {Meta} from "@storybook/react";
+
+const meta: Meta<typeof Circle> = {
+    component: Circle,
+    title: "Circle",
+};
+
+export default meta;
+
+export const BaseCircle = {};
---
+++ stories/Circle.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Circle = () => {
+    return (
+        <div className="bg-orange-500 w-14 p-2 rounded-full">
+        </div>
+    );
+};
+
+export default Circle;
