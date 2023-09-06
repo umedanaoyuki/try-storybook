856e66cd6954a51d30bc86062fb6504d21476f37
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-07 00:03:08 +0900

    円の描画

--- stories/Circle.tsx
+++ stories/Circle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Circle = () => {
     return (
-        <div className="bg-orange-500 w-14 p-2 rounded-full">
+        <div className="bg-orange-500 w-14 h-14 p-2 rounded-full">
         </div>
     );
 };
