135108072f2b6489ce43b716cb1a705c50d95d23
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-06 22:41:27 +0900

    tailwindCSS導入完了

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
         <h1 className="text-3xl text-red-700">
-            Hello world! Test
+            Hello world!
         </h1>
     </>
   )
